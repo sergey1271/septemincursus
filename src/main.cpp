6826f05fb5d059f7a52a75380f899d9619451cef
sergey1271 <samsunprob@gmail.com>
2026-03-11 21:01:21 +0300

    2

--- src/main.cpp
+++ src/main.cpp
@@ -1,142 +1,121 @@
 #include <Arduino.h>
-
 #include <BLEDevice.h>
 #include <BLEServer.h>
-#include <BLE2902.h>
 #include <BLEUtils.h>
+#include <BLE2902.h>
 
-#define NAME_DEVICE "ESP_BLE"
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
-#define SERVICE_CONTROL_UUID   "6f59f19e-2f39-49de-8525-5d2045f4d999" 
-#define SERVICE_WORK_TIME_UUID "f790145d-61dd-4464-9414-c058448ee9f2"
+// Настройки BLE
+BLEServer *pServer = nullptr;
+BLECharacteristic *pTemperatureCharacteristic = nullptr;
+BLECharacteristic *pHumidityCharacteristic = nullptr;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
 
-#define CONTROL_REQUEST_UUID "420ece2e-c66c-4059-9ceb-5fc19251e453"
-#define CONTROL_RESPONSE_UUID "a9bf2905-ee69-4baa-8960-4358a9e3a558"
+const int LEDq = 2; // Пин предустановленного на плату светодиода
+unsigned long previousMillis = 0;
+const long interval = 1000; // Интервал обновления показаний (мс)
 
-#define WORK_TIME_UUID "5e9f22d4-a305-4113-8fa2-55c5d497c3f2"
+// UUID для сервиса и характеристик
+#define SERVICE_UUID "6f59f19e-2f39-49de-8525-5d2045f4d999"
+#define HUMIDITY_UUID "cba1d466-344c-4be3-ab3f-189f80dd7518"
 
-#define PIN_LED_FIRST  25
-#define PIN_LED_SECOND 26
-#define PIN_SERVO      27
+// ИЗ СТАРОЙ ПРОГРАММЫ
+// #define SERVICE_CONTROL_UUID   "6f59f19e-2f39-49de-8525-5d2045f4d999" 
+// #define SERVICE_WORK_TIME_UUID "f790145d-61dd-4464-9414-c058448ee9f2"
 
-#define CMD_ENABLE_LED  0x1
-#define CMD_SERVO_ANGLE 0x2 
+// #define CONTROL_REQUEST_UUID "420ece2e-c66c-4059-9ceb-5fc19251e453"
+// #define CONTROL_RESPONSE_UUID "a9bf2905-ee69-4baa-8960-4358a9e3a558"
 
-BLECharacteristic* controlRequest;
-BLECharacteristic* controlResponse;
-BLECharacteristic* workTime;
+// #define WORK_TIME_UUID "5e9f22d4-a305-4113-8fa2-55c5d497c3f2"
 
-unsigned long timerMillis = 0;
-bool isConnected = false;
+// Класс для обработки событий BLE
+class MyServerCallbacks : public BLEServerCallbacks
+{
+    void onConnect(BLEServer *pServer)
+    {
+        deviceConnected = true;
+        Serial.println("Устройство ВКЛючено");
+        digitalWrite(LEDq, HIGH);
+    }
 
-void setupBluetooth();
-void setupLeds();
-void enableLed(int pin, bool enable);
-void sendWorkedTime(int seconds);
-void sendDeviceState();
+    void onDisconnect(BLEServer *pServer)
+    {
+        deviceConnected = false;
+        Serial.println("Устройство ОТКЛючено");
+        digitalWrite(LEDq, LOW);
 
-
-class BluetoothServerEventCallback : public BLEServerCallbacks {
-  void onConnect(BLEServer * server) 
-  {
-    isConnected = true;
-    sendDeviceState(); 
-  } 
-  void onDisconnect(BLEServer * server) 
-  {
-    isConnected = false;
-  }
+        pServer->startAdvertising(); // Перезапуск рекламы для повторного подключения
+    }
 };
 
-class BluetoothEventCallback : public BLECharacteristicCallbacks {
-  void onWrite(BLECharacteristic* characteristic) 
-  {
-    uint8_t* data = characteristic->getData();
-    if (data[0] == CMD_ENABLE_LED) { 
-      enableLed(data[1], data[2] > 0); 
-    } else if (data[0] == CMD_SERVO_ANGLE)
-    sendDeviceState();
-  }
-}; 
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Launching...");
-
-  Serial1.begin(115200, SERIAL_8N1, -1, -1, true, 20000UL);
-
-
-  setupBluetooth();
-  setupLeds(); 
-}
-
-void loop() { 
-  if (millis() - timerMillis > 5000) {
-    timerMillis = millis();
-
-    sendWorkedTime(timerMillis / 1000); 
-  } 
-}
-
-void setupBluetooth() 
+void setup()
 {
-  BLEDevice::init(NAME_DEVICE);
+    Serial.begin(115200);
+    pinMode(LEDq, OUTPUT);
 
-  BLEServer* server = BLEDevice::createServer();
-  server->setCallbacks(new BluetoothServerEventCallback());
-  BLEService* serviceControl  = server->createService(SERVICE_CONTROL_UUID);
-  controlRequest = serviceControl->createCharacteristic(CONTROL_REQUEST_UUID, BLECharacteristic::PROPERTY_WRITE);
-  controlRequest->setCallbacks(new BluetoothEventCallback());
-  controlResponse = serviceControl->createCharacteristic(CONTROL_RESPONSE_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
-  controlResponse->addDescriptor(new BLE2902()); 
+    // Настройка BLE
+    BLEDevice::init("ESP32-SHT30-BLE");
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
 
-  BLEService* serviceWorkTime = server->createService(SERVICE_WORK_TIME_UUID);
-  workTime = serviceWorkTime->createCharacteristic(WORK_TIME_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
-  workTime->addDescriptor(new BLE2902());
+    // Создание BLE-сервиса
+    BLEService *pService = pServer->createService(SERVICE_UUID);
 
-  serviceControl->start();
-  serviceWorkTime->start(); 
 
-  BLEAdvertising* advertising = BLEDevice::getAdvertising();
-  advertising->addServiceUUID(SERVICE_CONTROL_UUID);
-  advertising->addServiceUUID(SERVICE_WORK_TIME_UUID);
-  advertising->setMinPreferred(0x06); 
-  advertising->setMinPreferred(0x12);
-  advertising->start();  
+    // Запуск сервиса
+    pService->start();
+
+    // Начало рекламы устройства
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(0x06); // Параметры для улучшения совместимости
+    pAdvertising->setMinPreferred(0x12);
+    BLEDevice::startAdvertising();
+
+    Serial.println("BLE-сервер запущен. Ожидание подключений...");
 }
 
-void setupLeds() 
+void loop()
 {
-  pinMode(PIN_LED_FIRST, OUTPUT);
-  pinMode(PIN_LED_SECOND, OUTPUT);
-}
+    // Обработка подключения/отключения
+    if (!deviceConnected && oldDeviceConnected)
+    {
+        delay(500); // Даем время для стабилизации соединения
+        oldDeviceConnected = deviceConnected;
+    }
+    if (deviceConnected && !oldDeviceConnected)
+    {
+        oldDeviceConnected = deviceConnected;
+    }
 
+    // Обновление по таймеру
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval)
+    {
+        previousMillis = currentMillis;
 
-void enableLed(int pin, bool enable) 
-{
-  digitalWrite(pin, (enable) ? HIGH : LOW);
-}
+        if (!isnan(t) && !isnan(h))
+        {
+            Serial.printf("Temp: %.2f°C, Hum: %.2f%%\n", "g", "g");
 
-
-
-void sendWorkedTime(int seconds)
-{
-  if (isConnected) {
-    workTime->setValue(seconds);
-    workTime->notify();
-  }
-}
-
-void sendDeviceState() 
-{
-  if (isConnected) {
-    uint8_t data[5];
-    data[0] = PIN_LED_FIRST;                    
-    data[1] = digitalRead(PIN_LED_FIRST);
-    data[2] = PIN_LED_SECOND; 
-    data[3] = digitalRead(PIN_LED_SECOND);
-    controlResponse->setValue(data, 5);
-    controlResponse->notify();
-    Serial.printf("LED %d: %d, LED %d: %d, Servo: %d\n", data[0], data[1], data[2], data[3], data[4]);
-  }
+            // Отправка по BLE
+            if (deviceConnected)
+            {
+               
+                dtostrf("g", 5, 2, tempStr);
+                dtostrf("g" 5, 2, humStr);
+            }
+            else
+            {
+                Serial.println("Нет подключения к устройству");
+            }
+        }
+        else
+        {
+            Serial.println("Ошибка чтения данных с датчика");
+        }
+    }
 }
